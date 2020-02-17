ecd63382bd0f59d0aec07131a9e4397ce2d2b5ac
xyz13 <xyz13>
2020-02-17 14:18:49 +0200

    #BG-5060

--- include/Point.h
+++ include/Point.h
@@ -13,55 +13,55 @@ namespace influxdb
 {
 
 /// \brief Represents a point
-class Point
-{
-  public:
-    /// Constructs point based on measurement name
-    Point(const std::string& measurement);
+    class Point
+    {
+    public:
+        /// Constructs point based on measurement name
+        Point(const std::string& measurement);
 
-    /// Default destructor
-    ~Point() = default;
+        /// Default destructor
+        ~Point() = default;
 
-    /// Adds a tags
-    Point&& addTag(std::string_view key, std::string_view value);
+        /// Adds a tags
+        Point&& addTag(std::string_view key, std::string_view value);
 
-    /// Adds filed
-    Point&& addField(std::string_view name, std::variant<int, long long int, std::string, double> value);
+        /// Adds filed
+        Point&& addField(const std::string& name, double value);
 
-    /// Generetes current timestamp
-    static auto getCurrentTimestamp() -> decltype(std::chrono::system_clock::now());
+        /// Generetes current timestamp
+        static auto getCurrentTimestamp() -> decltype(std::chrono::system_clock::now());
 
-    /// Converts point to Influx Line Protocol
-    std::string toLineProtocol() const;
+        /// Converts point to Influx Line Protocol
+        std::string toLineProtocol() const;
 
-    /// Sets custom timestamp
-    Point&& setTimestamp(std::chrono::time_point<std::chrono::system_clock> timestamp);
+        /// Sets custom timestamp
+        Point&& setTimestamp(std::chrono::time_point<std::chrono::system_clock> timestamp);
 
-    /// Name getter
-    std::string getName() const;
+        /// Name getter
+        std::string getName() const;
 
-    /// Timestamp getter
-    std::chrono::time_point<std::chrono::system_clock> getTimestamp() const;
+        /// Timestamp getter
+        std::chrono::time_point<std::chrono::system_clock> getTimestamp() const;
 
-    /// Fields getter
-    std::string getFields() const;
+        /// Fields getter
+        std::string getFields() const;
 
-  protected:
-    /// A value
-    std::variant<long long int, std::string, double> mValue;
+    protected:
+        /// A value
+        std::variant<long long int, std::string, double> mValue;
 
-    /// A name
-    std::string mMeasurement;
+        /// A name
+        std::string mMeasurement;
 
-    /// A timestamp
-    std::chrono::time_point<std::chrono::system_clock> mTimestamp;
+        /// A timestamp
+        std::chrono::time_point<std::chrono::system_clock> mTimestamp;
 
-    /// Tags
-    std::string mTags;
+        /// Tags
+        std::string mTags;
 
-    /// Fields
-    std::string mFields;
-};
+        /// Fields
+        std::string mFields;
+    };
 
 } // namespace influxdb
 
